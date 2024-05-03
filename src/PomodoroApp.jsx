043cb5d1f6c610cf7f58ed8087e485c98ac5d1e9
GarooEBM <garoo.ebm1@gmail.com>
2024-05-03 02:56:42 -0600

    Creacion de proyecto

---
+++ src/PomodoroApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PomodoroPage } from './pomodoro/pages/PomodoroPage'
+
+export const PomodoroApp = () => {
+  return (
+    <>
+        <h1>PomodoroApp</h1>
+
+        <PomodoroPage />
+    </>
+  )
+}
